60d620e5ac6f9c4cec50c906314c8bdcdc86ee37
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2023-06-19 10:37:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 'use strict';
-let channelsPanel,channelsButton,channelsFunction;
-channelsPanel = document.getElementById('channelsPanel');
-channelsButton = document.querySelector('.channelsButton');
-
-channelsFunction=()=>{
-  if(channelsPanel.dataset.condition === 'minimized') channelsPanel.setAttribute('data-condition','maximized');
-  else channelsPanel.setAttribute('data-condition','minimized');
-};
+let sidePanelsToggleButton = document.querySelectorAll('.sidePanelsToggleButton');
 
 /*
 let app;
@@ -25,31 +18,12 @@ const resizeApp = new ResizeObserver(entries=>{
 
 resizeApp.observe(app);
 */
-channelsButton.addEventListener('click',()=>{
-  channelsFunction();
-  //resizeApp.unobserve(app);
+
+sidePanelsToggleButton.forEach(function(el) {
+  el.addEventListener('click',()=>{
+    //resizeApp.unobserve(app);
+    if(el.parentElement.dataset.condition === 'minimized') el.parentElement.setAttribute('data-condition','maximized');
+    else el.parentElement.setAttribute('data-condition','minimized');
+
+  });
 });
-
-let channelsBodyTitle,channelsTitleFunction;
-channelsBodyTitle = document.querySelectorAll('.channelsBodyTitle');
-//channelsBodyTitle.forEach(channelsTitleFunction);
-
-channelsTitleFunction=()=>{
-  
-};
-
-
-
-/*
-// scroll to top
-let upButton,template;
-upButton = document.querySelector('.button[data-type="up"]');
-template = document.getElementById('template');
-upButton.addEventListener('click',()=>template.scrollTo(0,0));
-*/
-
-//function loadHTML(){
-//  fetch('home.html')
-//  .then(response=> response.text())
-//  .then(text=> document.getElementById('homePage').innerHTML = text);
-//}
