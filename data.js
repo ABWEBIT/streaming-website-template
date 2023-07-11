b11cb5d07ffa58cf6ed9a81ac46716e553ff2385
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2023-07-11 20:44:30 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+'use strict';
+// избранное
+new Channel(
+  'listFavorites','online','123456',
+  'Алёша Попович и Тугарин Змей',
+  'Этот стример думает, что вам интересно смотреть');
+
+new Channel(
+  'listFavorites','online','12345',
+  'Колобок',
+  '✅ Заголовок трансляции');
+
+new Channel(
+  'listFavorites','online','1234',
+  'СИНЬОР ПОМИДОР',
+  'ОЧЕНЬ ДЛИННЫЙ ЗАГОЛОВОК ТРАНСЛЯЦИИ, ЗАХОДИ ПОСМОТРЕТЬ');
+
+new Channel(
+  'listFavorites','offline','0',
+  'Домовёнок Кузя',
+  '');
+
+new Channel(
+  'listFavorites','offline','0',
+  'Кот Леопольд',
+  '');
+
+// рекомендуемое
+new Channel(
+  'listRecommend','online','123456',
+  'Доктор Айболит',
+  'Добрый доктор');
+
+new Channel(
+  'listRecommend','online','12345',
+  'Дюймовочка',
+  'Маленькая и добрая девочка');
+
+new Channel(
+  'listRecommend','online','1234',
+  'Леший',
+  'Лесной дух');
+
+new Channel(
+  'listRecommend','online','123',
+  'Змей-горыныч',
+  'Огнедышащий дракон');
+
+new Channel(
+  'listRecommend','online','12',
+  'Кащей бессмертный',
+  'Злой чародей');
